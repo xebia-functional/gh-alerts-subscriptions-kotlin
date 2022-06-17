7d41afc3d93794cc0b62f4320f9bfb83d913a917
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-06-17 19:19:55 +0200

    Update repository insert query

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ application {
 sqldelight {
   database("SqlDelight") {
     packageName = "alerts.sqldelight"
-    dialect = libs.sqldelight.postgresql.asString
+    dialect(libs.sqldelight.postgresql.asString)
   }
 }
 
@@ -33,7 +33,7 @@ allprojects {
 repositories {
   mavenCentral()
   // For Kotest Extensions Arrow Fx, remove if 1.1.3 is released
-  maven(url= "https://oss.sonatype.org/content/repositories/snapshots")
+  maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 jib {
@@ -58,8 +58,6 @@ tasks {
       jvmTarget = "${JavaVersion.VERSION_11}"
       freeCompilerArgs = freeCompilerArgs + "-Xcontext-receivers" + "-opt-in=kotlin.RequiresOptIn"
     }
-    sourceCompatibility = "${JavaVersion.VERSION_11}"
-    targetCompatibility = "${JavaVersion.VERSION_11}"
   }
 
   test {
