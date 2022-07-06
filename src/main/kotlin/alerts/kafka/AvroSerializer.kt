e16a932dff552cbb2bfa6968b804c9cad216655e
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-07-06 12:40:28 +0200

    Add SubscriptionProducer, and AvroSerializer

--- src/main/kotlin/alerts/kafka/AvroSerializer.kt
+++ src/main/kotlin/alerts/kafka/AvroSerializer.kt
@@ -5,17 +5,22 @@ import kotlinx.serialization.KSerializer
 import org.apache.kafka.common.serialization.Deserializer
 import org.apache.kafka.common.serialization.Serializer
 
-class AvroSerialiser<A>(
-  val avro: Avro = Avro.default,
-  val serializer: KSerializer<A>,
+class AvroSerializer<A>(
+  private val serializer: KSerializer<A>,
+  private val avro: Avro = Avro.default,
 ) : Serializer<A>, Deserializer<A> {
-
+  
   override fun deserialize(topic: String?, data: ByteArray): A =
     avro.decodeFromByteArray(serializer, data)
-
+  
   override fun serialize(topic: String?, data: A): ByteArray =
     avro.encodeToByteArray(serializer, data)
-
-  override fun configure(configs: MutableMap<String, *>?, isKey: Boolean) {}
-  override fun close() {}
+  
+  @Suppress("EmptyFunctionBlock")
+  override fun configure(configs: MutableMap<String, *>?, isKey: Boolean) {
+  }
+  
+  @Suppress("EmptyFunctionBlock")
+  override fun close() {
+  }
 }
--- src/main/kotlin/alerts/kafka/SubscriptionProducer.kt
+++ src/main/kotlin/alerts/kafka/SubscriptionProducer.kt
@@ -3,14 +3,52 @@ package alerts.kafka
 import alerts.env.Env
 import alerts.persistence.Repository
 import arrow.fx.coroutines.Resource
+import arrow.fx.coroutines.continuations.resource
+import arrow.fx.coroutines.fromAutoCloseable
+import com.github.avrokotlin.avro4k.AvroNamespace
+import io.github.nomisRev.kafka.KafkaProducer
+import io.github.nomisRev.kafka.ProducerSettings
+import io.github.nomisRev.kafka.sendAwait
+import kotlinx.serialization.Serializable
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
 
-interface SubscriptionProduce {
-  suspend fun publish(repo: Repository): Unit
-  suspend fun delete(repo: Repository): Unit
+@Serializable
+@AvroNamespace("alerts.domain.subscription")
+data class SubscriptionKey(val repository: Repository)
+
+@Serializable
+@AvroNamespace("alerts.domain.subscription")
+enum class SubscriptionEvent {
+  Created, Deleted;
 }
 
-fun subscriptionProducer(
-  config: Env.Kafka,
-): Resource<SubscriptionProduce> {
-  TODO()
+interface SubscriptionProducer {
+  suspend fun publish(repo: Repository): Unit
+  suspend fun delete(repo: Repository): Unit
+  
+  companion object {
+    fun resource(kafka: Env.Kafka): Resource<SubscriptionProducer> = resource {
+      val settings = ProducerSettings(
+        bootstrapServers = kafka.bootstrapServers,
+        keyDeserializer = AvroSerializer(SubscriptionKey.serializer()),
+        valueDeserializer = AvroSerializer(SubscriptionEvent.serializer())
+      )
+      val producer = Resource.fromAutoCloseable { KafkaProducer(settings) }.bind()
+      DefaultSubscriptionProducer(producer, kafka.subscriptionTopic)
+    }
+  }
+}
+
+class DefaultSubscriptionProducer(
+  private val producer: KafkaProducer<SubscriptionKey, SubscriptionEvent>,
+  private val topic: Env.Kafka.Topic,
+) : SubscriptionProducer {
+  override suspend fun publish(repo: Repository): Unit {
+    producer.sendAwait(ProducerRecord(topic.name, SubscriptionKey(repo), SubscriptionEvent.Created))
+  }
+  
+  override suspend fun delete(repo: Repository) {
+    producer.sendAwait(ProducerRecord(topic.name, SubscriptionKey(repo), SubscriptionEvent.Deleted))
+  }
 }
